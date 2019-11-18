bfdce361f3ac6bb73f777eac7ae9b27fbaa3e96a
Clemens Riccabona <clemens@riccabona.it>
2019-11-18 14:57:05 +0100

    [TASK] Some more code cleanup

--- ext_autoload.php
+++ ext_autoload.php
@@ -1,5 +1,7 @@
 <?php
+
 $basePath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('alternet_beloginnews');
+
 return array(
     'AlterNET\AlternetBeloginnews\Utility\Display' => $basePath . 'Classes/Utility/Display.php'
 );
--- ext_localconf.php
+++ ext_localconf.php
@@ -2,9 +2,9 @@
 defined('TYPO3_MODE') or die();
 
 if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('scheduler')) {
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['AlterNET\\AlternetBeloginnews\\Task\\BeLoginNews'] = array(
-		'extension' => $_EXTKEY,
-		'title' => 'BE login news task',
-		'description' => 'Imports items from RSS feed into the BE login news',
-	);
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['AlterNET\\AlternetBeloginnews\\Task\\BeLoginNews'] = array(
+        'extension' => $_EXTKEY,
+        'title' => 'BE login news task',
+        'description' => 'Imports items from RSS feed into the BE login news',
+    );
 }
